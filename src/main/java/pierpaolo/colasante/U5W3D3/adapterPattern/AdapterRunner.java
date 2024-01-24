63a5fc91c64c7e868f6bf235947d6f36984908a5
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2024-01-24 16:08:52 +0100

    fk

--- src/main/java/pierpaolo/colasante/U5W3D3/adapterPattern/AdapterRunner.java
+++ src/main/java/pierpaolo/colasante/U5W3D3/adapterPattern/AdapterRunner.java
@@ -1,19 +1,22 @@
 package pierpaolo.colasante.U5W3D3.adapterPattern;
 
+import com.github.javafaker.Faker;
 import lombok.Builder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.Locale;
 
 @Component
 public class AdapterRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
+        Faker fk = new Faker(Locale.ITALY);
         Info info = new Info();
-        info.setNome("aldo");
-        info.setCognome("Baglio");
-        info.setDataDiNascita(LocalDate.of(1967,9,26));
+        info.setNome(fk.name().firstName());
+        info.setCognome(fk.name().lastName());
+        info.setDataDiNascita(LocalDate.of(1961,10,17));
 
         InfoAdapter infoAdapter = new InfoAdapter(info);
 
